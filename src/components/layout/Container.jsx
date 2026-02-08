7f6269b6613b75e40a27df916187bfff3a63e069
rubayet2027 <u2204002@student.cuet.ac.bd>
2026-02-08 22:57:34 +0600

    Fix Container component to accept className prop

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -1,7 +1,8 @@
-const Container = ({ children }) => (
-  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+const Container = ({ children, className = "" }) => (
+  <div className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ${className}`}>
     {children}
   </div>
 );
 
 export default Container;
+
